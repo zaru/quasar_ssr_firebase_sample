5b841becade198bb56c9b3537b00a2d4e6222fc8
zaru@sakuraba <zarutofu@gmail.com>
2018-10-14 17:22:23 +0900

    add SSR function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,4 @@
 const functions = require('firebase-functions');
+const server = require("./server/server.js");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.server = functions.https.onRequest(server.handler);
